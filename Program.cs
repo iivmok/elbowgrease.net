bd1d4b56698528fa7c437c1eeda2a88476ed3355
Iivari Mokelainen <iivmok@gmail.com>
2020-12-25 08:29:14 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace elbowgrease.net
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
